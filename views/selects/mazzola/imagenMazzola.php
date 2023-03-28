a2e9eb067f6b3b95a89a466b54f8d0a365977b7b
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 02:54:55 -0300

    user

--- views/selects/mazzola/imagenMazzola.php
+++ views/selects/mazzola/imagenMazzola.php
@@ -1,6 +1,6 @@
 <?php
 
 echo '<div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-<img src="https://i.ibb.co/R9rmwTp/mazzola.jpg" name="imagenMazzola" id="imagenMazzola"  alt="Mazzola" style="margin: 20px;border: 3px solid black;">
+<img src="https://i.ibb.co/R9rmwTp/mazzola.jpg" name="imagenMazzola" id="imagenMazzola"  alt="Mazzola" style="margin: 20px;border: 3px solid black; display:none;">
 </div>
 ';
