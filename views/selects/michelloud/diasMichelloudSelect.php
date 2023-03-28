fde41f5be745808afc8a4e26e566ba7d62da3716
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 18:55:15 -0300

    asd

--- views/selects/michelloud/diasMichelloudSelect.php
+++ views/selects/michelloud/diasMichelloudSelect.php
@@ -14,24 +14,27 @@ echo '<select name="diasMichelloudSelect" id="diasMichelloudSelect" class="form-
 // KK     $fecha = strtotime('+1 week', $fecha);
 // KK }
 
+date_default_timezone_set('America/Mexico_City');
+
+// Definimos la fecha actual
 $fecha_actual = time();
-    
-    // Definimos la fecha límite (un mes a partir de la fecha actual)
-    $fecha_limite = strtotime('+1 month', $fecha_actual);
-    
-    // Inicializamos la fecha en el primer miércoles a partir de la fecha actual
-    $fecha = strtotime('next Wednesday', $fecha_actual);
-    
-    // Recorremos las fechas hasta la fecha límite
-    while ($fecha <= $fecha_limite) {
-        // Mostramos la fecha en el formato deseado (día de la semana y día del mes en español)
-        $fecha_es = strftime('%A, %d de %B', $fecha);
-        
-        // Mostramos la opción del select
-        echo '<option value="'.date('Y-m-d', $fecha).'">'.$fecha_es.'</option>';
-        
-        // Avanzamos a la siguiente semana
-        $fecha = strtotime('next Wednesday', $fecha);
-    }
+
+// Definimos la fecha límite (un mes a partir de la fecha actual)
+$fecha_limite = strtotime('+1 month', $fecha_actual);
+
+// Inicializamos la fecha en el primer miércoles a partir de la fecha actual
+$fecha = strtotime('next Wednesday', $fecha_actual);
+
+// Recorremos las fechas hasta la fecha límite
+while ($fecha <= $fecha_limite) {
+    // Mostramos la fecha en el formato deseado (día de la semana y día del mes en español)
+    $fecha_es = strftime('%A, %d de %B', $fecha);
+
+    // Mostramos la opción del select
+    echo '<option value="' . date('Y-m-d', $fecha) . '">' . $fecha_es . '</option>';
+
+    // Avanzamos a la siguiente semana
+    $fecha = strtotime('+1 week', $fecha);
+}
 
 echo '</select>';
