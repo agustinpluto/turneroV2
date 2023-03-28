c95155fcb3f3170b0ab7802e09e076037d8a0e49
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 17:19:04 -0300

    Agrandando Logo

--- views/login.php
+++ views/login.php
@@ -105,7 +105,7 @@
         <main>
 
             <div class="container-fluid d-flex justify-content-center align-items-center flex-row">
-                <div class="container-fluid d-flex " style="background-color: #90559730">
+                <div class="container-fluid d-flex flex-row" style="background-color: #90559730">
                     <h1>TURNOS INTEGRA</h1>
                     <p class="fs-5 col-md-8">Centro de Rehabilitación Integral</p>
                 </div>
