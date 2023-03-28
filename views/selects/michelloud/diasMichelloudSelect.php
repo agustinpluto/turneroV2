9072b907eb73b12c3002576a80502d2e5bfe9228
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 19:02:43 -0300

    asd

--- views/selects/michelloud/diasMichelloudSelect.php
+++ views/selects/michelloud/diasMichelloudSelect.php
@@ -2,17 +2,7 @@
 
 echo '<select name="diasMichelloudSelect" id="diasMichelloudSelect" class="form-select form-select-lg mb-4"> <option value="no">Día</option>';
 
-// KK echo '';
-// KK $fechaActual = date('Y-m-d');
-// KK $fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
-// KK $fecha = strtotime('next Wednesday', strtotime($fechaActual));
-// KK 
-// KK while ($fecha <= strtotime('next Wednesday', strtotime($fechaDentroDeUnMes))) {
-// KK     $fecha_str = date('Y-m-d', $fecha);
-// KK     $fecha_fmt = date('l, d F', $fecha);
-// KK     echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
-// KK     $fecha = strtotime('+1 week', $fecha);
-// KK }
+
 setlocale(LC_TIME, "es_AR.UTF-8");
 // Definimos la fecha actual
 $fecha_actual = time();
@@ -26,7 +16,7 @@ $fecha = strtotime('next Wednesday', $fecha_actual);
 // Recorremos las fechas hasta la fecha límite
 while ($fecha <= $fecha_limite) {
     // Mostramos la fecha en el formato deseado (día de la semana y día del mes en español)
-    $fecha_es = strftime('%A, %d de %B', $fecha);
+    $fecha_es = strftime('%A, %d', $fecha);
 
     // Mostramos la opción del select
     echo '<option value="' . date('Y-m-d', $fecha) . '">' . ucfirst($fecha_es) . '</option>';
