37e8376665830a2a91ae95dfdb6deed366ddb8b8
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 18:53:29 -0300

    asd

--- views/selects/michelloud/diasMichelloudSelect.php
+++ views/selects/michelloud/diasMichelloudSelect.php
@@ -2,15 +2,36 @@
 
 echo '<select name="diasMichelloudSelect" id="diasMichelloudSelect" class="form-select form-select-lg mb-4"> <option value="no">Día</option>';
 
-echo '';
-$fechaActual = date('Y-m-d');
-$fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
-$fecha = strtotime('next Wednesday', strtotime($fechaActual));
+// KK echo '';
+// KK $fechaActual = date('Y-m-d');
+// KK $fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
+// KK $fecha = strtotime('next Wednesday', strtotime($fechaActual));
+// KK 
+// KK while ($fecha <= strtotime('next Wednesday', strtotime($fechaDentroDeUnMes))) {
+// KK     $fecha_str = date('Y-m-d', $fecha);
+// KK     $fecha_fmt = date('l, d F', $fecha);
+// KK     echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
+// KK     $fecha = strtotime('+1 week', $fecha);
+// KK }
 
-while ($fecha <= strtotime('next Wednesday', strtotime($fechaDentroDeUnMes))) {
-    $fecha_str = date('Y-m-d', $fecha);
-    $fecha_fmt = date('l, d F', $fecha);
-    echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
-    $fecha = strtotime('+1 week', $fecha);
-}
+$fecha_actual = time();
+    
+    // Definimos la fecha límite (un mes a partir de la fecha actual)
+    $fecha_limite = strtotime('+1 month', $fecha_actual);
+    
+    // Inicializamos la fecha en el primer miércoles a partir de la fecha actual
+    $fecha = strtotime('next Wednesday', $fecha_actual);
+    
+    // Recorremos las fechas hasta la fecha límite
+    while ($fecha <= $fecha_limite) {
+        // Mostramos la fecha en el formato deseado (día de la semana y día del mes en español)
+        $fecha_es = strftime('%A, %d de %B', $fecha);
+        
+        // Mostramos la opción del select
+        echo '<option value="'.date('Y-m-d', $fecha).'">'.$fecha_es.'</option>';
+        
+        // Avanzamos a la siguiente semana
+        $fecha = strtotime('next Wednesday', $fecha);
+    }
+
 echo '</select>';
