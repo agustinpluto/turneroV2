4713fcf1dc9fde5bd007524c1a558b76b37041e4
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 18:27:53 -0300

    Agrandando Logo

--- views/selects/michelloud/diasMichelloudSelect.php
+++ views/selects/michelloud/diasMichelloudSelect.php
@@ -3,7 +3,7 @@
 echo '<select name="diasMichelloudSelect" id="diasMichelloudSelect" class="form-select form-select-lg mb-4"> <option value="no">Día</option>';
 
 
-setlocale(LC_ALL, 'es_ES.UTF-8');
+setlocale(LC_ALL, 'es_AR.UTF-8');
 
 
 $fechaActual = date('Y-m-d');
