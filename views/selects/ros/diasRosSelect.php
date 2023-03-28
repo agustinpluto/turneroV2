6f40d90236cb211d3faea6cf4b67e3841d9d15ac
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 20:20:56 -0300

    asd

--- views/selects/ros/diasRosSelect.php
+++ views/selects/ros/diasRosSelect.php
@@ -1,17 +1,6 @@
 <?php
 echo '';
 echo '<select name="diasRosSelect" id="diasRosSelect" class="form-select form-select-lg mb-4" style="display: none"> <option value="no">Dia</option>';
-$fechaActual = date('Y-m-d');
-$fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
-$fecha = strtotime('next Friday', strtotime($fechaActual));
-
-while ($fecha <= strtotime('next Friday', strtotime($fechaDentroDeUnMes))) {
-    $fecha_str = date('Y-m-d', $fecha);
-    $fecha_fmt = date('l, d F', $fecha);
-    echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
-    $fecha = strtotime('+1 week', $fecha);
-}
-echo '</select>';
 
 setlocale(LC_TIME, "es_AR.UTF-8");
 $fecha_actual = time();
