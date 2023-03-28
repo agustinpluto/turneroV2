85525d485113df64a54853ea7bba1dc60ccb43aa
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 18:49:43 -0300

    asd

--- views/selects/michelloud/diasMichelloudSelect.php
+++ views/selects/michelloud/diasMichelloudSelect.php
@@ -2,18 +2,15 @@
 
 echo '<select name="diasMichelloudSelect" id="diasMichelloudSelect" class="form-select form-select-lg mb-4"> <option value="no">Día</option>';
 
-// Establecer la configuración regional en español
-setlocale(LC_TIME, 'es_ES.UTF-8');
+echo '';
+$fechaActual = date('Y-m-d');
+$fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
+$fecha = strtotime('next Wednesday', strtotime($fechaActual));
 
-// Definir la fecha actual y la fecha límite (30 días después)
-$hoy = new DateTime();
-$limite = new DateTime('+30 days');
-
-// Generar opciones para los martes y jueves hasta la fecha límite
-while ($hoy <= $limite) {
-    $dia_semana = $hoy->format('w');
-    if ($dia_semana == 4) {
-        echo '<option value="' . $hoy->format('Y-m-d') . '">' . ucfirst(strftime('%A, %d %B %Y', $hoy->getTimestamp())) . '</option>';
-    }
-    $hoy->modify('+1 day');
+while ($fecha <= strtotime('next Wednesday', strtotime($fechaDentroDeUnMes))) {
+    $fecha_str = date('Y-m-d', $fecha);
+    $fecha_fmt = date('l, d F', $fecha);
+    echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
+    $fecha = strtotime('+1 week', $fecha);
 }
+echo '</select>';
