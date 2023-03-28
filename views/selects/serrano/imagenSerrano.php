6ad166db7ff0192ce8a06bd8477b711dd1059557
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 02:48:34 -0300

    user

--- views/selects/serrano/imagenSerrano.php
+++ views/selects/serrano/imagenSerrano.php
@@ -1,6 +1,6 @@
 <?php
 
 echo '<div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-<img src="https://i.ibb.co/fr8Njm8/michelloud.jpg" name="imagenMichelloud" id="imagenMichelloud"  alt="Michelloud" style="margin: 20px;border: 3px solid black;">
+<img src="https://i.ibb.co/fr8Njm8/michelloud.jpg" name="imagenSerrano" id="imagenSerrano"  alt="Serrano" style="margin: 20px;border: 3px solid black;">
 </div>
 ';
