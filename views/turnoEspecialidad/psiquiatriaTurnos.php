59f1ef7b2db5d1ac2812fe4e046f17df0cc8fb37
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 16:11:39 -0300

    asd

--- views/turnoEspecialidad/psiquiatriaTurnos.php
+++ views/turnoEspecialidad/psiquiatriaTurnos.php
@@ -172,7 +172,8 @@ if (empty($id)) {
 
             <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
                 <button class="btn btn-lg btn-primary w-75 m-1" type="submit" name="botonRegistro" style="background-color: #905597;border-color: #8e8db7;">Agendar turno</button>
-                <a href="../administrador/index.php" class="btn btn-lg btn-primary w-75 m-1" type="submit" name="botonRegistro" style="background-color: white; border:2px solid #f2dc23;color: black;">Volver a las Especialidades</a>
+                <a href="../administrador/index.php" class="btn btn-lg btn-primary w-75 m-1" 
+                type="submit" name="botonRegistro" style="background-color: white; border:2px solid #f2dc23;color: black;">Volver</a>
             </div>
         </form>
     </main>
