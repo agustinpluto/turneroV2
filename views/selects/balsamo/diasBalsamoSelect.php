d4a5d33202a6f08d9a30a3eb1ab5a8994e8b9bbd
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 19:45:57 -0300

    asd

--- views/selects/balsamo/diasBalsamoSelect.php
+++ views/selects/balsamo/diasBalsamoSelect.php
@@ -1,14 +1,19 @@
 <?php
 echo '';
 echo '<select name="diasBalsamoSelect" id="diasBalsamoSelect" class="form-select form-select-lg mb-4" style="display: none"><option value="no">Día</option>';
-$fechaActual = date('Y-m-d');
-$fechaDentroDeUnMes = date('Y-m-d', strtotime('+1 month'));
-$fecha = strtotime('next Thursday', strtotime($fechaActual));
 
-while ($fecha <= strtotime('next Thursday', strtotime($fechaDentroDeUnMes))) {
-    $fecha_str = date('Y-m-d', $fecha);
-    $fecha_fmt = date('l, d F', $fecha);
-    echo "<option value=\"$fecha_str\">$fecha_fmt</option>";
+setlocale(LC_TIME, "es_AR.UTF-8");
+$fecha_actual = time();
+$fecha_limite = strtotime('+1 month', $fecha_actual);
+$fecha = strtotime('next Thursday', $fecha_actual);
+
+while ($fecha <= $fecha_limite) {
+    
+    $fecha_es = strftime('%A %d/%m', $fecha);
+
+    echo '<option value="' . date('Y-m-d', $fecha) . '">' . ucfirst($fecha_es) . '</option>';
+
     $fecha = strtotime('+1 week', $fecha);
 }
+
 echo '</select>';
