2426652d832d32ef987e9deaf2cc5dcdccfb2245
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 17:58:55 -0300

    Agrandando Logo

--- index.php
+++ index.php
@@ -139,144 +139,19 @@
           <p>Información detallada, días y horarios de atención</p>
           <div class="accordion accordion-flush" id="accordionFlushExample">
 
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingOne">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                  Fisiatría
-                </button>
-              </h2>
-              <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-
-
-                </div>
-              </div>
-            </div>
-
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                  Fonoaudiología
-                </button>
-              </h2>
-              <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingFour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                  Kinesiología
-                </button>
-              </h2>
-              <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingFive">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
-                  Neurología
-                </button>
-              </h2>
-              <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingSix">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
-                  Nutrición
-                </button>
-              </h2>
-              <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingSeven">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
-                  Psicología
-                </button>
-              </h2>
-              <div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingEight">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
-                  Psicomotricidad
-                </button>
-              </h2>
-              <div id="flush-collapseEight" class="accordion-collapse collapse" aria-labelledby="flush-headingEight" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingNine">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="false" aria-controls="flush-collapseNine">
-                  Psicopedagogía
-                </button>
-              </h2>
-              <div id="flush-collapseNine" class="accordion-collapse collapse" aria-labelledby="flush-headingNine" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingTen">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTen" aria-expanded="false" aria-controls="flush-collapseTen">
-                  Psiquiatría
-                </button>
-              </h2>
-              <div id="flush-collapseTen" class="accordion-collapse collapse" aria-labelledby="flush-headingTen" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingEleven">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEleven" aria-expanded="false" aria-controls="flush-collapseEleven">
-                  Terapia Ocupacional
-                </button>
-              </h2>
-              <div id="flush-collapseEleven" class="accordion-collapse collapse" aria-labelledby="flush-headingEleven" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
-            <div class="list-group-item">
-              <h2 class="accordion-header" id="flush-headingTwelve">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwelve" aria-expanded="false" aria-controls="flush-collapseTwelve">
-                  Trabajadora Social
-                </button>
-              </h2>
-              <div id="flush-collapseTwelve" class="accordion-collapse collapse" aria-labelledby="flush-headingTwelve" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-
-                </div>
-              </div>
-            </div>
+            <ul class="list-group">
+              <li class="list-group-item">Fisiatría</li>
+              <li class="list-group-item">Fonoaudiología</li>
+              <li class="list-group-item">Kinesiología</li>
+              <li class="list-group-item">Neurología</li>
+              <li class="list-group-item">Nutrición</li>
+              <li class="list-group-item">Psicología</li>
+              <li class="list-group-item">Psicomotricidad</li>
+              <li class="list-group-item">Psicopedagogía</li>
+              <li class="list-group-item">Psiquiatría</li>
+              <li class="list-group-item">Terapia Ocupacional</li>
+              <li class="list-group-item">Trabajadora Social</li>
+            </ul>
             <div class="list-group-item">
               <h2 class="accordion-header" id="flush-headingThirdteen">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThirdteen" aria-expanded="false" aria-controls="flush-collapseThirdteen">
