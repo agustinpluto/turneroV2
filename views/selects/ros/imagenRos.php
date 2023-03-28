5b94e66b124a24a16e3bd80f50d2cb10e06dc7e6
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 03:09:15 -0300

    user

--- views/selects/ros/imagenRos.php
+++ views/selects/ros/imagenRos.php
@@ -1,6 +1,6 @@
 <?php
 
 echo '<div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-<img src="https://i.ibb.co/QFbRSQL/prueba.jpg" name="imagenMichelloud" id="imagenMichelloud"  alt="Michelloud" style="margin: 20px;border: 3px solid black; display:none">
+<img src="https://i.ibb.co/QFbRSQL/prueba.jpg" name="imagenRos" id="imagenRos"  alt="Ros" style="margin: 20px;border: 3px solid black; display:none">
 </div>
 ';
