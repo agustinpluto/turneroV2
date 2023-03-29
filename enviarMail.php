8559cac5ee6c4eeb82c25a830dd5eb230d39f3a8
Agustin Pluto <agustinpluto@gmail.com>
2023-03-29 16:50:48 -0300

    mailin

--- enviarMail.php
+++ enviarMail.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -17,32 +18,33 @@ $verifcode = sha1(microtime(true));
 $to      = $email;
 $subject = 'Nuevo turno';
 $msg = "<html>
-Tenes un nuevo turno para el: ".$fecha."
-<br>Con el paciente: ".$paciente."
-<br>A la hora: ".$hora."
+Tenes un nuevo turno para el: " . $fecha . "
+<br>Con el paciente: " . $paciente . "
+<br>A la hora: " . $hora . "
 </html>";
 
-$headers = [
-    'From' => 'agustin no-reply@turnero-integra.com.ar',
-'content-type' => 'text/html',
-'MIME-Version' => '1.0',
-'Date' => date('r'),
-'Message-ID' => '<'.sha1(microtime(true)).'@turnero-integra.com.ar>'
-];
 
-mail_me($to, $subject, $msg, $headers);
 
-function mail_me($to, $subject, $message, $headers) {
+mail_me($to, $subject, $msg);
 
+function mail_me($to, $subject, $message)
+{
+    $headers = [
+        'From' => 'agustin no-reply@turnero-integra.com.ar',
+        'content-type' => 'text/html',
+        'MIME-Version' => '1.0',
+        'Date' => date('r'),
+        'Message-ID' => '<' . sha1(microtime(true)) . '@turnero-integra.com.ar>'
+    ];
     try {
         $mail = new PHPMailer(true);
-        $mail->isSMTP();  
+        $mail->isSMTP();
         $mail->SMTPDebug = 0;
         $mail->Host = 'smtp.sendgrid.net';
         $mail->SMTPAuth = true; // Enable SMTP authentication
         $mail->Username = 'apikey';
         $mail->Password = '';
-        $mail->SMTPSecure = 'tls';//PHPMailer::ENCRYPTION_STARTTLS; Enable TLS encryption,
+        $mail->SMTPSecure = 'tls'; //PHPMailer::ENCRYPTION_STARTTLS; Enable TLS encryption,
         $mail->Port = 587; // TCP port to connect to
         $mail->CharSet = 'UTF-8';
         $mail->isHTML(true); // Set email format to HTML
@@ -53,8 +55,8 @@ function mail_me($to, $subject, $message, $headers) {
         $mail->setFrom('no-reply@turnero-integra.com.ar', 'agustin');
         $mail->addAddress($to); // Add a recipient
         $mail->send();
-       echo 'Message has been sent';
+        echo 'Message has been sent';
     } catch (Exception $e) {
-       echo "{$mail->ErrorInfo}";
+        echo "{$mail->ErrorInfo}";
     }
 }
