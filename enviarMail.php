df46834cccd3d107a88a1123021f964f1308eb9f
Agustin Pluto <agustinpluto@gmail.com>
2023-03-29 00:07:00 -0300

    mailer

--- enviarMail.php
+++ enviarMail.php
@@ -17,11 +17,11 @@ mail_me($to, $subject, $msg, $headers);
 function mail_me($to, $subject, $message) {
 
 $headers = [
-        'From' => 'AGUS <no-reply@dominio_registrado_En_sendrgrid.com>',
+        'From' => 'AGUS <no-reply@em1753.turnero-integra.com.ar>',
     'content-type' => 'text/html',
     'MIME-Version' => '1.0',
     'Date' => date('r'),
-    'Message-ID' => '<'.sha1(microtime(true)).'@dominio_registrado_En_sendrgrid.com>'
+    'Message-ID' => '<'.sha1(microtime(true)).'@em1753.turnero-integra.com.ar>'
 ];
     try {
         $mail = new PHPMailer(true);
@@ -39,7 +39,7 @@ $headers = [
         $mail->Subject = $subject;
         $mail->Body = $message;
 
-        $mail->setFrom('<mail registrado en send grid>', 'Nombre');
+        $mail->setFrom('<no-reply@em1753.turnero-integra.com.ar>', 'AGUS');
         $mail->addAddress($to); // Add a recipient
         $mail->send();
        echo 'Message has been sent';
