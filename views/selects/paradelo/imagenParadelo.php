9847c769be2ef110b6ca24a0a2ab968b33ab5c5f
Agustin Pluto <agustinpluto@gmail.com>
2023-03-29 20:02:57 -0300

    mailin

--- views/selects/paradelo/imagenParadelo.php
+++ views/selects/paradelo/imagenParadelo.php
@@ -1,6 +1,6 @@
 <?php
 
 echo '<div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-<img src="https://i.ibb.co/DfvyWCV/Paradelo.jpg" name="imagenMichelloud" id="imagenMichelloud"  alt="Michelloud" style="margin: 20px;border: 3px solid black; display:none">
+<img src="https://i.ibb.co/DfvyWCV/Paradelo.jpg" name="imagenParadelo" id="imagenParadelo"  alt="Paradelo" style="margin: 20px;border: 3px solid black; display:none">
 </div>
 ';
