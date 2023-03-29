4663a7cfc6612d1cbc05c5d88a66a5f86db4b1d0
Agustin Pluto <agustinpluto@gmail.com>
2023-03-29 20:33:55 -0300

    mailin

--- views/turnoEspecialidad/depto1.php
+++ views/turnoEspecialidad/depto1.php
@@ -126,7 +126,7 @@ if (empty($id)) {
 
             <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
                 <button class="btn btn-lg btn-primary w-75 m-1" type="submit" name="botonRegistro" style="background-color: #905597;border-color: #8e8db7;">Solicitar turno</button>
-                <a href="../administrador/index.php" class="btn btn-lg btn-primary w-75 m-1" type="submit" name="botonRegistro" style="background-color: white; border:2px solid #f2dc23;color: black;">Volver a las Especialidades</a>
+                <a href="../pacientes/index.php" class="btn btn-lg btn-primary w-75 m-1" type="submit" name="botonRegistro" style="background-color: white; border:2px solid #f2dc23;color: black;">Volver a las Especialidades</a>
             </div>
         </form>
     </main>
