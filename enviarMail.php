6b2c2db08b20aec3da190c2532b444c340910262
Agustin Pluto <agustinpluto@gmail.com>
2023-03-28 23:20:18 -0300

    mailing

--- enviarMail.php
+++ enviarMail.php
@@ -1,23 +1,49 @@
 <?php
-// Uncomment next line if you're not using a dependency loader (such as Composer)
-require_once './sendgrid-php/sendgrid-php.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-use SendGrid\Mail\Mail;
+// getcomposer.org
+// composer update
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
-$email = new Mail();
-$email->setFrom("no-reply@centrointegra.com", "Integra");
-$email->setSubject("PRUEBA A A A ");
-$email->addTo("agustinpluto@gmail.com", "Agustin");
-$email->addContent("text/plain", "  HOLAAAAAAA ");
-$email->addContent(
-    "text/html", "<strong>PRUEBA EXITOSA</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '.  $e->getMessage(). "\n";
+$verifcode = sha1(microtime(true));
+$to      = 'fidosma@gmail.com';
+$subject = 'Verificar Cuenta para Turnero';
+$msg = "<html>Hola, hace click aca: https://lalala/verify/$verifcode</html>";
+
+mail_me($to, $subject, $msg, $headers);
+
+function mail_me($to, $subject, $message) {
+
+$headers = [
+        'From' => 'AGUS <no-reply@dominio_registrado_En_sendrgrid.com>',
+    'content-type' => 'text/html',
+    'MIME-Version' => '1.0',
+    'Date' => date('r'),
+    'Message-ID' => '<'.sha1(microtime(true)).'@dominio_registrado_En_sendrgrid.com>'
+];
+    try {
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();  
+        $mail->SMTPDebug = 0;
+        $mail->Host = 'smtp.sendgrid.net';
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'apikey';
+        $mail->Password = 'SG.alNLf_**************ujzaiJ9E-PYk';
+        $mail->SMTPSecure = 'tls';//PHPMailer::ENCRYPTION_STARTTLS; Enable TLS encryption,
+        $mail->Port = 587; // TCP port to connect to
+        $mail->CharSet = 'UTF-8';
+        $mail->isHTML(true); // Set email format to HTML
+
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        $mail->setFrom('<mail registrado en send grid>', 'Nombre');
+        $mail->addAddress($to); // Add a recipient
+        $mail->send();
+       echo 'Message has been sent';
+    } catch (Exception $e) {
+       echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
