cbff8cfced020ea562cc3aaeaf46b0227967bdb3
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 10:22:23 -0300

    mp

--- views/pacientes/abonar.php
+++ views/pacientes/abonar.php
@@ -144,12 +144,12 @@ if ($rol != 2 || empty($id)) {
 
         <div class="container-fluid">
             <div class="text-center">
-                <a href="./index.php" class="btn btn-primary btn-lg w-100 mb-1" style="background-color: #905597;border-color: #8e8db7;">Inicio</a>
-                <a href="./turnos.php" class="btn btn-primary btn-lg w-100 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Turnos</a>
-                <a href="./pagos.php" class="btn btn-primary btn-lg w-100 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Pagos</a>
-                <a href="./perfil.php" class="btn btn-primary btn-lg w-100 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Datos</a>
-                <a href="./abonar.php" class="btn btn-primary btn-lg w-100 mb-1" style="background-color: #905597;border-color: #8e8db7;">Abonar Seña</a>
-                <a href="../../funciones/logout.php" class="btn btn-primary btn-lg px-4 mx-3" style="background-color: #905597;border-color: #8e8db7;">Cerrar sesión</a>
+                <a href="./index.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Inicio</a>
+                <a href="./turnos.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Mis Turnos</a>
+                <a href="./pagos.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Mis Pagos</a>
+                <a href="./perfil.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Mis Datos</a>
+                <a href="./abonar.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Abonar Seña</a>
+                <a href="../../funciones/logout.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Cerrar sesión</a>
             </div>
         </div>
         <main class="form-signin">
