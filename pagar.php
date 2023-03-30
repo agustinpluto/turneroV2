7be8b13b9a81223bc8283b0e3ea6368e8066ee83
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 05:57:26 -0300

    mp

--- pagar.php
+++ pagar.php
@@ -2,7 +2,7 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('TEST-2146525193346993-033003-535aa38b949446a9f6d34b6fa6820bf0-83722175');
+MercadoPago\SDK::setAccessToken('');
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
