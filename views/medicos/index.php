47e3b3f427e30b4d0637347e0eb1c5fd01cce29a
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 00:31:50 -0300

    mailin

--- views/medicos/index.php
+++ views/medicos/index.php
@@ -129,7 +129,7 @@ if ($rol != 3 || empty($id)) {
 
       <form action="get">
         <input class="form-controls" name="busqueda" placeholder="Buscar por apellido">
-        <button class="btn btn-primary">Buscar</button>
+        <button class="btn btn-danger m-1">Buscar</button>
       </form>
 
       <?php
