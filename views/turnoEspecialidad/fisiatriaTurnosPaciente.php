52ac90e8cedb9e27479284374c166a2030061452
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 06:13:48 -0300

    mp

--- views/turnoEspecialidad/fisiatriaTurnosPaciente.php
+++ views/turnoEspecialidad/fisiatriaTurnosPaciente.php
@@ -9,7 +9,7 @@ if ($rol != 2 || empty($id)) {
 }
 
 // SDK de Mercado Pago
-require '../../' . __DIR__ .  '/vendor/autoload.php';
+require '../../vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('TEST-2146525193346993-033003-535aa38b949446a9f6d34b6fa6820bf0-83722175');
 // Crea un objeto de preferencia
