c328a3458aed41b6abc494ca6893acaebf94cbef
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 08:14:17 -0300

    mp

--- views/pacientes/perfil.php
+++ views/pacientes/perfil.php
@@ -142,7 +142,7 @@ if ($rol != 2 || empty($id)) {
             </div>
         </div>
 
-        <div class="mb-5 d-flex col-sm-1 col-md-1">
+        <div class="container-fluid">
             <a href="../../funciones/logout.php" class="btn btn-primary btn-lg px-4 mx-3" style="background-color: #905597;border-color: #8e8db7;">Cerrar sesión</a>
             <a href="./turnos.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis turnos</a>
             <a href="./perfil.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis datos</a>
