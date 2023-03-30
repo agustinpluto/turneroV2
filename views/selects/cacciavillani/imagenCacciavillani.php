38bb4804708595f587e005fa4cc804503a889144
Agustin Pluto <agustinpluto@gmail.com>
2023-03-29 21:10:22 -0300

    mailin

--- views/selects/cacciavillani/imagenCacciavillani.php
+++ views/selects/cacciavillani/imagenCacciavillani.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-<img src="https://i.ibb.co/NTKnkbt/correa.jpg" name="imagenCacciavillani" id="imagenCacciavillani"  alt="Cacciavillani" style="margin: 20px;border: 3px solid black; display:none">
+<img src="https://i.ibb.co/vJBV3BP/cacciavillani.jpg" name="imagenCacciavillani" id="imagenCacciavillani"  alt="Cacciavillani" style="margin: 20px;border: 3px solid black; display:none">
 </div>
 ';
 
