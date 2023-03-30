9df103c971f16bdbabb70e337004e525c8ba8de3
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 00:26:03 -0300

    mailin

--- views/medicos/index.php
+++ views/medicos/index.php
@@ -128,8 +128,10 @@ if ($rol != 3 || empty($id)) {
     <div class="row g-5 d-flex">
 
         <div class="mb-6">
+          <form action="get">
           <input type="text" class="form-control" name="busqueda" id="busqueda" placeholder="Buscar por apellido">
           <a name="button" id="button" class="btn btn-primary" href="#" role="button">Button</a>
+          </form>
         </div>
 
       <?php
