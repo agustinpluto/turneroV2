2faa45995195fc9e3236b9085eba0e54874bd188
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 10:03:47 -0300

    mp

--- views/pacientes/abonar.php
+++ views/pacientes/abonar.php
@@ -161,14 +161,14 @@ if ($rol != 2 || empty($id)) {
                 <div class="container-fluid d-flex justify-content-center align-items-center">
                     <div class="col col-sm-1 w-50 h-50 justify-content-center align-items-center">
                         
-                            <a href="./abonarEvaluacion.php" class="card-body">
+                            <a href="./abonarEvaluacion.php" class="container">
                                 <h3 class="card-title text-center">Señar Consulta</h3>
                                 <p class="card-text text-center">$2000</p>
                                 <p class="card-text text-center">Débito / Crédito</p>
                             </a>
                     </div>
                     <div class="col col-sm-1 w-50 h-50 justify-content-center align-items-center">
-                            <a href="./abonarEvaluacion.php" class="card-body">
+                            <a href="./abonarEvaluacion.php" class="container">
                                 <h3 class="card-title text-center">Señar Evaluación</h3>
                                 <p class="card-text text-center">$7500</p>
                                 <p class="card-text text-center">Débito / Crédito</p>
