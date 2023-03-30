0087c220b394d504a2434db3e239dbd5aafed1b6
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 10:23:36 -0300

    mp

--- views/pacientes/abonar.php
+++ views/pacientes/abonar.php
@@ -143,7 +143,7 @@ if ($rol != 2 || empty($id)) {
         </div>
 
         <div class="container-fluid">
-            <div class="text-center">
+            <div class="d-flex justify-content-center">
                 <a href="./index.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Inicio</a>
                 <a href="./turnos.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Mis Turnos</a>
                 <a href="./pagos.php" class="btn btn-primary btn-lg flex-fill mx-1" style="background-color: #905597;border-color: #8e8db7;">Mis Pagos</a>
