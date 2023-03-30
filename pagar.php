784f0dcaf82bacbd0877064958490f54d8865696
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 11:52:03 -0300

    mp

--- pagar.php
+++ pagar.php
@@ -6,6 +6,7 @@ $payment = $_GET['payment_id'];
 $status = $_GET['status'];
 $payment_type = $_GET['payment_type'];
 $order_id = $_GET['merchant_order_id'];
+date_default_timezone_set('America/Argentina/Buenos_Aires');
 $fecha_hora = date("Y-m-d H:i:s");
 
 include "./database/conexion.php";
