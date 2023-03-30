c981d2f7582fc4ce4a78d935be6bb809fd6a29fd
Agustin Pluto <agustinpluto@gmail.com>
2023-03-30 09:44:46 -0300

    mp

--- views/pacientes/turnos.php
+++ views/pacientes/turnos.php
@@ -147,7 +147,7 @@ if ($rol != 2 || empty($id)) {
         <div class="container-fluid">
             <a href="./index.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Inicio</a>
 
-            <a href="./turnos.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Turnos</a>
+            <a href="./turnos.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1 active" style="background-color: #905597;border-color: #8e8db7;">Mis Turnos</a>
             <a href="./pagos.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Pagos</a>
             <a href="./perfil.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Mis Datos</a>
             <a href="./abonar.php" class="btn btn-primary btn-lg px-4 mx-3 mb-1" style="background-color: #905597;border-color: #8e8db7;">Abonar Seña</a>
